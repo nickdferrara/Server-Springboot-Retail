f5ad172e4ab2b1badcd98f99594bfe81f1ce4ef6
Nicholas <nferrara@stewartsshops.com>
2024-10-16 16:28:09 -0400

    Fix modularity tests

--- src/test/kotlin/com/nickdferrara/retailstore/ModularityTests.kt
+++ src/test/kotlin/com/nickdferrara/retailstore/ModularityTests.kt
@@ -9,31 +9,11 @@ class ModularityTests {
 
     @Test
     fun `verify modular structure`() {
-        modules.verify()
+        ApplicationModules.of(Application::class.java).verify()
     }
 
     @Test
     fun `create documentation`() {
         Documenter(modules).writeModuleCanvases()
-    }
-
-    @Test
-    fun `verify orders module`() {
-        modules.getModuleByName("orders").ifPresent { it.verify() }
-    }
-
-    @Test
-    fun `verify fulfillment module`() {
-        modules.getModuleByName("fulfillment").ifPresent { it.verify() }
-    }
-
-    @Test
-    fun `verify shipping module`() {
-        modules.getModuleByName("shipping").ifPresent { it.verify() }
-    }
-
-    @Test
-    fun `verify accounting module`() {
-        modules.getModuleByName("accounting").ifPresent { it.verify() }
     }
 }
