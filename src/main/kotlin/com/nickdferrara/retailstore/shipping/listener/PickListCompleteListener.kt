042cce7f4c1d084f2694b07efcb86d88a05d1ae2
Nicholas Ferrara <nick.d.ferrara@gmail.com>
2024-10-17 21:05:52 -0400

    Make pick list complete listener logic

--- src/main/kotlin/com/nickdferrara/retailstore/shipping/listener/PickListCompleteListener.kt
+++ src/main/kotlin/com/nickdferrara/retailstore/shipping/listener/PickListCompleteListener.kt
@@ -1,17 +1,21 @@
 package com.nickdferrara.retailstore.shipping.listener
 
 import com.nickdferrara.retailstore.fulfillment.events.PickListCompleteEvent
+import com.nickdferrara.retailstore.shipping.mapper.ShippingRouteMapper
 import com.nickdferrara.retailstore.shipping.service.ShippingRouteService
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class PickListCompleteListener(
-    private val shippingRouteService: ShippingRouteService
+    private val shippingRouteService: ShippingRouteService,
+    private val shippingRouteMapper: ShippingRouteMapper
+
 ) {
 
     @EventListener
     fun handlePickListCompleteEvent(event: PickListCompleteEvent) {
-
+        val shippingRoute = shippingRouteMapper.toShippingRoute(event.pickList)
+        shippingRouteService.createShippingRoute(shippingRoute)
     }
 }
